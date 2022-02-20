85c7429384fe0c22e0b66b3c1c3142a30a012407
Peter Esezobor <70745960+peteCoder@users.noreply.github.com>
2022-02-20 16:38:08 +0100

    my commit

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
-import Head from 'next/head'
-import Header from '../components/Header'
-import Nav from '../components/Nav'
-import Results from '../components/Results'
-import requests from '../utils/requests'
+import Head from 'next/head';
+import Header from '../components/Header';
+import Nav from '../components/Nav';
+import Results from '../components/Results';
+import requests from '../utils/requests';
 
 export default function Home({results}) {
   console.log(results)
@@ -13,13 +13,10 @@ export default function Home({results}) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      {/* Header Component */}
       <Header />
 
-      {/* Navbar Component */}
       <Nav />
 
-      {/* Result Component */}
       <Results results={results} />
     </>
     
